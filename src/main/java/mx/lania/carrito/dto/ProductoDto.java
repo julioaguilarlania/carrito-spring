f3b5c21e39e63a9002849712c0f4b373b981bbbb
Julio César Aguilar <julio.aguilar@lania.edu.mx>
2025-11-05 23:33:15 -0600

    Corrige nombre propiedad idProducto

--- src/main/java/mx/lania/carrito/dto/ProductoDto.java
+++ src/main/java/mx/lania/carrito/dto/ProductoDto.java
@@ -3,18 +3,18 @@ package mx.lania.carrito.dto;
 import java.math.BigDecimal;
 
 public class ProductoDto {
-    private Long iProducto;
+    private Long idProducto;
     private String nombre;
     private String descripcion;
     private BigDecimal precio;
     private Integer cantidadDisponible;
 
-    public Long getiProducto() {
-        return iProducto;
+    public Long getIdProducto() {
+        return idProducto;
     }
 
-    public void setiProducto(Long iProducto) {
-        this.iProducto = iProducto;
+    public void setIdProducto(Long iProducto) {
+        this.idProducto = iProducto;
     }
 
     public String getNombre() {
