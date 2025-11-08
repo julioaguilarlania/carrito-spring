67d3552eea87e1ab3189dd64b19d4fcd0538b858
Julio César Aguilar <julio.aguilar@lania.edu.mx>
2025-11-07 21:20:55 -0600

    Ajustes en mappers de Carrito y Detalle

--- src/main/java/mx/lania/carrito/mappers/MapperCarrito.java
+++ src/main/java/mx/lania/carrito/mappers/MapperCarrito.java
@@ -1,17 +1,36 @@
 package mx.lania.carrito.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import mx.lania.carrito.dto.CarritoDto;
+import mx.lania.carrito.dto.DetalleCarritoDto;
 import mx.lania.carrito.entidades.Carrito;
+import mx.lania.carrito.entidades.DetalleCarrito;
 
 @Mapper
 public interface MapperCarrito {
     
+    @Mapping(source = "items", target = "items", qualifiedByName = "detallesToDto")
     CarritoDto toDto(Carrito Carrito);
 
     @Mapping(target = "idCliente", ignore = true)
+    @Mapping(source = "items", target = "items", qualifiedByName = "detallesToEntity")
     Carrito toEntity(CarritoDto CarritoDto);
+
+    @Named("detallesToDto")
+    default List<DetalleCarritoDto> detallesToDto(List<DetalleCarrito> detalles) {
+        MapperDetalleCarrito mapper = MapperDetalleCarrito.INSTANCE;
+        return mapper.toDtoList(detalles);
+    }
+
+    @Named("detallesToEntity")
+    default List<DetalleCarrito> detallesToEntity(List<DetalleCarritoDto> detallesDto) {
+        MapperDetalleCarrito mapper = MapperDetalleCarrito.INSTANCE;
+        return mapper.toEntityList(detallesDto);
+    }
 
 }
--- src/main/java/mx/lania/carrito/mappers/MapperDetalleCarrito.java
+++ src/main/java/mx/lania/carrito/mappers/MapperDetalleCarrito.java
@@ -1,7 +1,10 @@
 package mx.lania.carrito.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 import mx.lania.carrito.dto.DetalleCarritoDto;
 import mx.lania.carrito.entidades.DetalleCarrito;
@@ -9,7 +12,13 @@ import mx.lania.carrito.entidades.DetalleCarrito;
 @Mapper
 public interface MapperDetalleCarrito {
     
+    MapperDetalleCarrito INSTANCE = Mappers.getMapper(MapperDetalleCarrito.class);
+
     DetalleCarritoDto toDto(DetalleCarrito detalle);
+
     @Mapping(target = "idCarrito", ignore = true)
     DetalleCarrito toEntity(DetalleCarritoDto detalleDto);
+
+    List<DetalleCarritoDto> toDtoList(List<DetalleCarrito> detalles);
+    List<DetalleCarrito> toEntityList(List<DetalleCarritoDto> detallesDto);
 }
