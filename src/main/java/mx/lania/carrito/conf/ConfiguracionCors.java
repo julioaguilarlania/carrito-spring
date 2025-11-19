78c739095309d2e90074a68afbc53c7c17f13e9c
Julio Cesar Aguilar Cabrera <julio.aguilar@lania.edu.mx>
2025-11-19 15:04:42 -0600

    Agrega configuracion de CORS para acceso desde Angular

---
+++ src/main/java/mx/lania/carrito/conf/ConfiguracionCors.java
@@ -0,0 +1,22 @@
+package mx.lania.carrito.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfiguracionCors {
+    
+    @Bean
+    public WebMvcConfigurer configuracioConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:4200")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
